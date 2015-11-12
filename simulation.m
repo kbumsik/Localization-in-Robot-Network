4ac8bfb8e6a280f1a6d2ccdc451e717934e7242b
unknown <BUMSKIM@LC02.campus.stonybrook.edu>
2015-11-12 13:07:58 -0500

    Simulation: Models for Free Space Path loss and Additive Gaussian White Noise added. Robot class: merged x and y member to a vector. Reference: Gaussian Noise added.

--- simulation.m
+++ simulation.m
@@ -1,14 +1,24 @@
 %% Firstly clear all
 clc;   clear all;   close all;
 
+%==========================================================================
+
+%% Initialize
+
 % a little change
 %$ set constants
-xField = 600;
-yField = 600;
-sens = 80;
-comm = 50;
-reject = 40;
-numOfObjects = 8;
+xField = 600;       % in meters
+yField = 600;       % in meters
+sens = 80;          % in meters
+comm = 50;          % in meters
+reject = 40;        % in meters
+numOfObjects = 2;   % number of robots
+Pt = 100;           % in decibel(db)
+freq = 2000000000;	% in hertz =>2Gigahertz
+CONST_C = 299792458;    % speed of light (m/s)
+sigmaNoise = 2;     % standard deviation parameter of the gaussian noise
+
+%===================================
 
 %% Create objects
 for i = 1: numOfObjects;
@@ -21,7 +31,7 @@ for i = 1: numOfObjects;
             randX = sens+rand()*(xField-2*sens);
             randY = sens+rand()*(yField-2*sens);
            for j = 1:i-1;
-               result = (randX-robot(j).x)^2 + (randY-robot(j).y)^2 - (reject*2)^2;
+               result = (randX-robot(j).getX)^2 + (randY-robot(j).getY)^2 - (reject*2)^2;
               if ( result < 0 )
                   check = 1;
               end
@@ -34,12 +44,32 @@ for i = 1: numOfObjects;
     
 end
 
+%==========================================================================
+
 %% Display
 field = figure(1);
-set(field,'position',[200,200,xField,yField]);
+set(field,'position',[200,200,xField*1.1,yField*1.1]); % set window size
+
+ 
 plot(0)
 hold on;
 for i = 1: numOfObjects;
     drawAll(robot(i));
+    
 end
 hold off;
+set (gca,'xlim',[0 xField],'ylim',[0 yField], ...
+    'xtick', 0:50:xField, 'ytick', 0:50:yField); % set the limit of the plot
+
+
+%==========================================================================
+
+%% Calculate the signal strength
+distance = norm(robot(1).position-robot(2).position);
+line([robot(1).getX robot(2).getX], [robot(1).getY robot(2).getY]);
+
+%  Measureing power of signal using path loss
+Pr = Pt -20*log10(distance)-20*log10(freq)-20*log10(4*pi/CONST_C);
+
+% Adding Additive gaussian noise
+Pr = Pr + normrnd(0,2) % adding a random number from normal distribution
