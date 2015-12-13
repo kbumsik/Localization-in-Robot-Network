be19bb87b29341c37aae29b39402eb4f1a6ff4b9
aawjjaa <weijiejie3@gmail.com>
2015-12-13 18:18:54 -0500

    report added

---
+++ random_signal_test.m
@@ -0,0 +1,15 @@
+%% ==========================================================================
+x = [0:0.01:10];
+    p1 = raylpdf(x,0.5);
+    p2 = raylpdf(x,1);
+    p3 = raylpdf(x,2);
+    plot(x,p1,x,p2,x,p3);
+%%
+%rayleigh distrubution simulate
+pd=makedist('Rayleigh');
+hold on;
+ for x1 = 1: 1001;
+    y(x1)=random(pd); 
+ end
+ plot(x1,y+sin(x1));
+hold off;
